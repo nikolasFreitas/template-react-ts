e9b6eaaf1d3da805be032e36d5b3053040d3b2ed
Nikolas Freitas <nikolasbvf@gmail.com>
2020-05-23 01:44:20 -0300

    :sparkles: Start eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './screens/Home';
+import ParagrafoExemplo from './screens/Home/style';
 
 function App() {
   return (
@@ -8,16 +10,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Home />
+        <ParagrafoExemplo />
       </header>
     </div>
   );
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+  const message = 'hello world';
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+};
