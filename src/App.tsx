0bbd04c4430bf1ae0c8fa95487727a67188d9036
Nikoals Freitas <nikolasbvf@gmail.com>
2020-05-28 23:04:20 -0300

    Feature/create router (#2)
    * Fix eslint browser variables
    
    * Init router of application
    
    * Don't allow javascript to application in tsconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Home from './screens/Home';
-import ParagrafoExemplo from './screens/Home/style';
+import AppRouter from './router';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <Home />
-        <ParagrafoExemplo />
+        <AppRouter />
       </header>
     </div>
   );
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import ParagrafoExemplo from './style';
 
 export default () => {
   const message = 'hello world';
   return (
     <div>
       <h1>{message}</h1>
+      <ParagrafoExemplo />
     </div>
   );
 };
