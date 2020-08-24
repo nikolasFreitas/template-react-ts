8d48334a93dd98cadef0fafcf1c9e474df7ceca7
Nikolas Freitas <nikolas.freitas@poatek.com>
2020-08-24 02:07:14 -0300

    Remove not used import

--- src/screens/Login/hooks.tsx
+++ src/screens/Login/hooks.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { LoginApi } from '../../service';
 import ApiRequestHooks from '../../service/apiRequestHooks';
